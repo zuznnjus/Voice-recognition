a031c35ea00bf333a3a47aae79d8a394be97764a
Juszczak <juszczakzuzanna99@gmail.com>
2021-01-18 22:41:15 +0100

    Add try except

--- inf141238_inf141240.py
+++ inf141238_inf141240.py
@@ -3,81 +3,51 @@ from scipy.signal import decimate
 from copy import copy
 import numpy as np
 import warnings
-import os
-import re
-import matplotlib.pyplot as plt
+import sys
+
 
 def getSignalData(w, signal):
     n = signal.shape[0]
     frequencies = np.arange(n) / n * w
-    signal_kaiser = signal * np.kaiser(n, 100)
+    signal_kaiser = signal * np.kaiser(n, 20)
     signal_kaiser_fft = abs(np.fft.fft(signal_kaiser)) / (0.5 * n)
 
     return frequencies, signal_kaiser_fft
 
-def harmonicProductSpectrum(frequencies, signal_fft):
-    fig = plt.figure(figsize=(8, 10), dpi=80)
-    ax = fig.add_subplot(5, 1, 1)
-    ax.plot(frequencies, signal_fft, '-')
-    ax.set_xlim(0, 1000)
 
+def harmonicProductSpectrum(frequencies, signal_fft):
     signal_hps = copy(signal_fft)
+
     for i in np.arange(2, 6):
         dec = decimate(signal_fft, i)
         signal_hps[:len(dec)] *= dec
-        ax = fig.add_subplot(5, 1, i)
-        ax.plot(frequencies, signal_hps, '-')
-        ax.set_xlim(0, 1000)
 
     left_boundary = np.where(frequencies > 20)[0][0]
     right_boundary = np.where(frequencies > 1000)[0][0]
-    maxfreqq = frequencies[left_boundary:right_boundary][np.argmax(signal_hps[left_boundary:right_boundary])]
-    print(maxfreqq)
-    plt.show()
-    return maxfreqq
+
+    return (frequencies[left_boundary:right_boundary])[np.argmax(signal_hps[left_boundary:right_boundary])]
+
 
 def main():
-    warnings.filterwarnings('ignore')
+    try:
+        warnings.filterwarnings('ignore')
 
-    #filename = 'trainall/001_K.wav'
-    all = 0
-    recognised = 0
-    unrecognised = []
-
-    for file in os.listdir("trainproblematic2"):
-        if file.endswith(".wav"):
-            filename = file
-            all += 1
-
-        if re.match("\\d{3}_K.wav", filename):
-            gender = 'K'
-        elif re.match("\\d{3}_M.wav", filename):
-            gender = 'M'
-
-        w, signal = wavfile.read("trainproblematic2/" + filename)
+        filename = sys.argv[1]
+        w, signal = wavfile.read(filename)
 
         if len(signal.shape) > 1:     # only first channel
             signal = signal[:, 0]
 
         frequencies, signal_fft = getSignalData(w, signal)
         max_freq = harmonicProductSpectrum(frequencies, signal_fft)
-        # print(max_freq)
 
         if max_freq < 170:
-            print(filename + ' M')
-            if gender == 'M':
-                recognised += 1
-            else:
-                unrecognised.append(filename)
+            print('M')
         else:
-            print(filename + ' M')
-            if gender == 'K':
-                recognised += 1
-            else:
-                unrecognised.append(filename)
+            print('K')
+    except:
+        print('K')
 
-    print(recognised*100/all)
-    print(len(unrecognised))
 
 if __name__ == '__main__':
     main()
